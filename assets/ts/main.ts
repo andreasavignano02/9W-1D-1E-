0093c3f9b0c780652a74a59ed4001c91b869c99d
andreasavignano02 <112469730+andreasavignano02@users.noreply.github.com>
2022-10-24 17:48:02 +0200

    fix finale

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -6,13 +6,23 @@ console.log(giocatore2);
 let numeroRandom:number = (function generatoreNumero(){
     let numero = (Math.random() * (100-1) + 1);
     let numeroDecimale = Math.floor(numero);
-    console.log(numeroDecimale)
-    return numeroDecimale
+    console.log(numeroDecimale);
+    return numeroDecimale;
 })()
 
-if (giocatore1 === numeroRandom || giocatore2 === numeroRandom){
-    console.log("Hai indovianto")
+
+function confronto(){
+    let diff1 = Math.abs(numeroRandom - giocatore1);
+    let diff2 = Math.abs(numeroRandom - giocatore2);
+
+if (giocatore1 === numeroRandom){
+    console.log("Hai indovianto 1")
     } 
-    else {
-    console.log("Avete sbagliato entrambi")
+    else if (giocatore1 === numeroRandom){
+    console.log("Hai indovinato 2")
+} else if (diff1 < diff2){
+    console.log("giocatore 1 è più vicino")
+}else {
+    console.log("giocatore 2 è più vicino")
+}
 }
